e1590591bc96a4761b4435ed997b5a0e4a4838b0
dannyjay1996 <7iajun4n@gmail.com>
2023-05-24 21:06:54 +0800

    version 0524

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,19 +12,16 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Jiajun An</h2>
-        <p>7iajun4n@gmail.com</p>
+        <h2>Jiajun An (Danny)</h2>
+        <ContactIcons />
+        <h3>The platformization of robotics is ushering in a new era.</h3>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
-        and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
-        I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
-        , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
-        {' '}<a href="https://planet.com">Planet</a> and <a href="https://facebook.com">Facebook</a>.
-      </p>
+      <p>Hi, I&apos;m Jiajun An (Danny), a Ph.D. graduate from the <a href="https://biomedirobotics.com/">BMT group</a> at the MAE Department, <a href="https://en.wikipedia.org/wiki/Chinese_University_of_Hong_Kong">The Chinese University of Hong Kong</a>, under the supervision of <a href="https://www4.mae.cuhk.edu.hk/peoples/au-kwok-wai-samuel/">Prof. AU Kwok Wai Samuel</a>. Previously, I earned my M.Sc. degree from <a href="https://en.wikipedia.org/wiki/Purdue_University">Purdue University</a> in USA and my B.E. degree from <a href="https://en.wikipedia.org/wiki/Shanghai_Jiao_Tong_University">Shanghai Jiao Tong University</a> in China.</p>
+      <p>I am a roboticist with my focus on mechanical design and hardware development.</p>
       <ul className="actions">
         <li>
           {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
@@ -33,8 +30,7 @@ const SideBar = () => (
     </section>
 
     <section id="footer">
-      <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Jiajun An, Last updated on May 2023</p>
     </section>
   </section>
 );
